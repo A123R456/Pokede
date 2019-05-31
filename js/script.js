95d57585b98d65e3441c460349ecf1cd7da01f9c
A123R456 <Angheloromero123@gmail.com>
2019-05-31 12:26:07 -0600

    falta

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 var Pokemon=angular.module("Pokemon",[])
 Pokemon.controller("Pokedex",function($scope,$rootScope,$http){
+   $scope.PokedexB = [];
+
+   for (var i = 0; i  < 100; i++) {
    $http({
    	method: "GET",
-   	url:"https://pokeapi.co/api/v2/pokemon/20"
+   	url:"https://pokeapi.co/api/v2/pokemon/"+i
    }).then(function callbackSuccess(y){
-     console.log(y);
+     $scope.PokedexB.push(y);
 
    })	
 	
+
+	}
 })
